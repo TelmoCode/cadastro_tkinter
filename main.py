61a9ff53cd7eaef4ca4a1db4fc9d782371bd0e9a
TelmoCode <telmogferreira@gmail.com>
2023-02-24 22:45:43 -0300

    função

--- main.py
+++ main.py
@@ -2,32 +2,44 @@ import tkinter as tk
 import datetime as dt
 from tkinter import ttk
 
-lista_tipos = ['caderno','lapis','Caneta','borracha']
+lista_tipos = ['caderno', 'lapis', 'Caneta', 'borracha']
+
+
+# função de inserção
+
+def inserir():
+    descricao = entry_descricao.get()
+    tipo = cb_selecionar.get()
+    quant = entry_quant.get()
+
 
 janela = tk.Tk()
 
 janela.title('Cadastro de produtos')
 
+# Descrição
 lbl_descricao = tk.Label(text='Descrição dos materiais')
-lbl_descricao.grid(row=1, column=0, pady=10, padx=10, columnspan=4, sticky='nswe')
+lbl_descricao.grid(row=1, column=0, pady=10, padx=10, columnspan=1, sticky='nswe')
 
 entry_descricao = tk.Entry()
 entry_descricao.grid(row=2, column=0, pady=10, padx=10, columnspan=4, sticky='nswe')
 
+# Tipo
 tipo_material = tk.Label(text='Tipo de material')
-tipo_material.grid(row=3, column=0, pady=10, padx=10, columnspan=2, sticky='nswe')
+tipo_material.grid(row=3, column=0, pady=10, padx=10, columnspan=1, sticky='nswe', )
 
 cb_selecionar = ttk.Combobox(values=lista_tipos)
 cb_selecionar.grid(row=3, column=2, pady=10, padx=10, columnspan=2, sticky='nswe')
 
+# Quantidade
 lbl_quant = tk.Label(text='Quantidade dos materiais')
 lbl_quant.grid(row=4, column=0, pady=10, padx=10, columnspan=2, sticky='nswe')
 
 entry_quant = tk.Entry()
-entry_quant.grid(row=5, column=0, pady=10, padx=10, columnspan=4, sticky='nswe')
+entry_quant.grid(row=4, column=2, pady=10, padx=10, columnspan=4, sticky='nswe')
 
-btn_cadastrar = ttk.Button(text='Cadastrar')
-btn_cadastrar.grid(row=6, column=0, pady=10, padx=10, columnspan=4, sticky='nswe')
-
+# Botao
+btn_cadastrar = tk.Button(text='Cadastrar')
+btn_cadastrar.grid(row=6, column=0, pady=10, padx=10, columnspan=1, sticky='nswe')
 
 janela.mainloop()
