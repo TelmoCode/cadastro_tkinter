be8f1c9ec0946258bf8f54e3ac8166a484642cd4
TelmoCode <telmogferreira@gmail.com>
2023-02-24 21:21:12 -0300

    botoes

--- main.py
+++ main.py
@@ -20,4 +20,14 @@ tipo_material.grid(row=3, column=0, pady=10, padx=10, columnspan=2, sticky='nswe
 cb_selecionar = ttk.Combobox(values=lista_tipos)
 cb_selecionar.grid(row=3, column=2, pady=10, padx=10, columnspan=2, sticky='nswe')
 
+lbl_quant = tk.Label(text='Quantidade dos materiais')
+lbl_quant.grid(row=4, column=0, pady=10, padx=10, columnspan=2, sticky='nswe')
+
+entry_quant = tk.Entry()
+entry_quant.grid(row=5, column=0, pady=10, padx=10, columnspan=4, sticky='nswe')
+
+btn_cadastrar = ttk.Button(text='Cadastrar')
+btn_cadastrar.grid(row=6, column=0, pady=10, padx=10, columnspan=4, sticky='nswe')
+
+
 janela.mainloop()
